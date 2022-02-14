40ebb820dc4d2df9d66b36516f6499be974362b0
Yessikag <4760@holbertonschool.com>
2022-02-13 23:05:35 -0500

    prints the alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,13 +2,13 @@
 #include <ctype.h>
 /*
  */int main(void)
- {
-	 int x;
-	 for (x = 'A'; x <= 'Z'; x++)
-	 {
-		 int lower_x = tolower(x);
-		 putchar(lower_x);
-	 }
-	 putchar('\n');
-	 return (0);
- }
+{
+	int x;
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+		int lower_x = tolower(x);
+		putchar(lower_x);
+	}
+	putchar('\n');
+	return (0);
+}
