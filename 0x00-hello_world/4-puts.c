b15d234a0c50ec1ae4e3d2fa15d2bbabfb48131a
Yessikag <4760@holbertonschool.com>
2022-02-14 19:03:24 -0500

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /*
  *not sure what i think of betty */ int main(void)
- {
-	 puts("\"Programming is like building a multilingual puzzle");
-	 return (0);
- }
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
