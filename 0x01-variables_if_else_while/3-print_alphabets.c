46241133ffa8fd6b31bfdb84946d9733333f278b
Yessikag <4760@holbertonschool.com>
2022-02-14 22:05:23 -0500

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,15 +3,15 @@
 /*
  */int main(void)
  {
-	int x;
-	for (x = 'a'; x <= 'z'; x++)
-	{
-		putchar(x);
-	}
-	for (x = 'A'; x <= 'Z'; x++)
-	{
-		putchar(x);
-	}
-	putchar('\n');
-	return (0);
+	 int x;
+	 for (x = 'a'; x <= 'z'; x++)
+	 {
+		 putchar(x);
+	 }
+	 for (x = 'A'; x <= 'Z'; x++)
+	 {
+		 putchar(x);
+	 }
+	 putchar('\n');
+	 return (0);
 }
