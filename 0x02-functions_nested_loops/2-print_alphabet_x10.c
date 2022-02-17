f3d4e7453d7e77d1b2d9855c80036f1b2d789fdf
Yessikag <4760@holbertonschool.com>
2022-02-16 22:50:02 -0500

    print

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /*
  */void print_alphabet_x10(void)
- {
-	 int x, p;
-	 for (p = 0; p < 10; p++)
-	 {
-		 for (x = 'a'; x <= 'z'; x++)
-			 _putchar(x);
-		 _puthcar('\n');
-	 }
- }
+{
+	int x, p;
+	for (p = 0; p < 10; p++)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+			_putchar(x);
+		_puthcar('\n');
+	}
+}
