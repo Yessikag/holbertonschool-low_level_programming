21864322a40fbcfc8db3df0be4d4e32ce50f46ed
Yessikag <4760@holbertonschool.com>
2022-02-16 22:29:35 -0500

    print

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,12 @@
 #include "main.h"
-
-/**
- * main - check the code.
- *
- * Return: Always 0.
- */
-void print_alphabet_x10(void)
-	int y;
-for(y=0; y<=10;y++)
-{
-	print_alphabet_x10();
-	return (0);
-}
+/*
+ */void print_alphabet_x10(void)
+ {
+	 int x, p;
+	 for (p = 0; p < 10; p++)
+	 {
+		 for (x = 'a'; x <= 'z'; x++)
+			 _putchar(x);
+		 _puthcar('\n');
+	 }
+ }
